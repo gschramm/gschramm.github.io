d42a8106087c7c0eac08a8c4bbe051aed4184cec
Georg Schramm <georg.schramm@kuleuven.be>
2024-03-04 10:25:52 +0100

    update scholar script

--- scripts/gen_publist_from_scholar.py
+++ scripts/gen_publist_from_scholar.py
@@ -1,6 +1,7 @@
-import bibtexparser
 from scholarly import scholarly
 from habanero import Crossref
+
+# TODO: set ENTRYTYPE (article, inproceedings, etc) and ID (unique identifier) for each entry
 
 # %%
 # Retrieve the author's data, fill-in, and print
@@ -19,31 +20,38 @@ print()
 author = scholarly.fill(first_author_result )
 
 # %%
-publication_filled = [scholarly.fill(x) for x in author['publications']]
-bibs = [pub['bib'] for pub in publication_filled]
 
-# %%
-# use cross-ref API to find DOIs
 cr = Crossref()
 
-for ib, bib in enumerate(bibs):
-    query = f"{bib['title']}, {bib['citation']}"
-    res = cr.works(query=query)
-    i = 0
-
-    for it in res['message']['items']:
-        if it['title'][0].lower()[:10] == bib['title'].lower()[:10]:
-            print(ib, it['author'][0]['family'])
-            print(it['title'][0])
-            if 'DOI' in it: 
-                print(it['DOI'])
-                bib['doi'] = it['DOI']
-                bib['dimensions'] = 'true'
-                bib['altmetric'] = 'true'
-            print()
-            break
-
-    bib['bibtex_show'] = 'true'
-
 with open('bibtex.bib', 'w') as bibtex_file:
-    bibtexparser.dump(bibs[0], bibtex_file)
+    for pub in author['publications']:
+        scholarly.fill(pub)
+    
+        bib = pub['bib']
+        bib['ENTRYTYPE'] = 'article'
+        bib['ID'] = pub['cites_id'][0]
+        bib.pop('abstract', None)
+    
+        # find the DOI from crossref
+        query = f"{bib['title']}, {bib['citation']}"
+        res = cr.works(query=query)
+        i = 0
+    
+        for it in res['message']['items']:
+            if it['title'][0].lower()[:10] == bib['title'].lower()[:10]:
+                print(it['author'][0]['family'])
+                print(it['title'][0])
+                if 'DOI' in it: 
+                    print(it['DOI'])
+                    bib['doi'] = it['DOI']
+                    bib['dimensions'] = 'true'
+                    bib['altmetric'] = 'true'
+                    bib['html'] = f"https://doi.org/{bib['doi']}"
+                print()
+                break
+    
+        bib['bibtex_show'] = 'true'
+    
+        bib_str = scholarly.bibtex(pub)
+        print(bib_str)
+        bibtex_file.write(bib_str)
